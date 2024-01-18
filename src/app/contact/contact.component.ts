374a70fa84110da5e79600064d2aecb87c92b19e
Mohamed Yahya Badra <m.badra@riekerworld.com>
2024-01-18 08:07:14 +0100

    add page story

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-
+  formData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+  constructor(private http: HttpClient) {}
+  onSubmit() {
+    this.http
+      .post('http://localhost:3000/submit_form', this.formData)
+      .subscribe(
+        () => {
+          console.log('Form submitted successfully');
+          this.formData = {
+            name: '',
+            email: '',
+            message: '',
+          };
+        },
+        (error) => {
+          console.error('Error submitting form:', error);
+          this.formData = {
+            name: '',
+            email: '',
+            message: '',
+          };
+        }
+      );
+  }
 }
