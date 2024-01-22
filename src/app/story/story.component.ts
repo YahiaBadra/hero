0cff63d32a0f6efa40c33fed6f6e261e2a0128f6
Mohamed Yahya Badra <m.badra@riekerworld.com>
2024-01-22 08:07:55 +0100

    fix page story

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SharedService } from '../shared.service';
+import { Hero } from '../card-image/hero.module';
 
 @Component({
   selector: 'app-story',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./story.component.css']
 })
 export class StoryComponent {
+  id?: number;
+  route = inject(ActivatedRoute);
+  sharedService = inject(SharedService);
+  description?: string;
+  imgUrl?: string;
+  name?: string;
+  constructor() {
+    this.id = parseInt(this.route.snapshot.paramMap.get('id')!);
+
+    this.sharedService.getHeroById(this.id).subscribe({
+      next: (response: Hero) => {
+        this.description = response.description;
+        this.imgUrl = response.imgUrl;
+        this.name = response.name;
+      },
+    });
+  }
 
 }
