cdea09c3386bf7f078cd7df84d80113ad20dd9c7
Mohamed Yahya Badra <m.badra@riekerworld.com>
2024-02-02 08:08:42 +0100

    handel the pages

--- src/app/card-image/card-image.component.ts
+++ src/app/card-image/card-image.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { Hero } from './hero.module';
 import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-card-image',
@@ -17,6 +18,8 @@ export class CardImageComponent implements OnInit {
   private route = inject(ActivatedRoute);
 
   heroes: Hero[] = [];
+  paragraph1?: any;
+  paragraph2?: any;
 
   ngOnInit(): void {
     this.shared.fetchHeroes().subscribe((response: Hero[]) => {
@@ -26,6 +29,17 @@ export class CardImageComponent implements OnInit {
       //   .filter((hero) => hero !== null)
       //   .map((heroObj) => Object.values(heroObj)[0]);
     });
+    this.paragraph1 = `In narratology and comparative mythology, the hero's journey, also
+    known as the monomyth, is the common template of stories that involve
+    a hero who goes on an adventure, is victorious in a decisive crisis,
+    and comes home changed or transformed.`;
+
+    this.paragraph2 = `The people of Ashkavor crowded around the temple square, eager to
+    witness the ascension of their new guardian -- to stand near the man
+    as he bound their souls to his own. But as his final brush strokes
+    fell against the runestone, and the bond of a new Ascended One was
+    forged, everyone -- even those who'd stayed in their homes -- could
+    sense that something had gone terribly wrong.`;
   }
 
   details(id: number) {
